a29624d89babff65a4556a7007c413f5bc90d913
jonpena <gran_200014@hotmail.com>
2023-05-05 16:47:50 -0400

    clon de youtube primer commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { SearchBar } from "./";
+
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      background: "#101010",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img
+        className="ml-2"
+        src="./public/youtube-icon.png"
+        alt="logo"
+        width={48}
+        height={48}
+      />
+      <span className="text-white font-semibold text-xl ml-2">YuoTubo</span>
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
---
+++ src/containers/Layout.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import { Navbar } from "../components";
+
+const Layout = ({ ...props }) => {
+  return (
+    <Box sx={{ backgroundColor: "#101010", height: "100vh" }}>
+      <Box className="max-w-screen-2xl bg-[#101010]">
+        <Navbar />
+        {props.children}
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
